0ee04ea76a58ccb3a0ff7e095b332267494af116
Ewan Bauld <ewan.bauld@gmail.com>
2019-01-15 22:50:49 +0000

    Add further tests to App

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -17,12 +17,40 @@ describe('App', () => {
           expect(app.find('Form').exists()).toBe(true);
       });
 
-      it('should redner a form control component', () => {
+      it('should render a form control component', () => {
           expect(app.find('FormControl').exists()).toBe(true);
       });
       it('should render a submit button', () => {
           expect(app.find('.btn').at(0).text()).toEqual('Submit');
       });
     })
-    
+    describe('when creating a note', () => {
+      let testNote = 'test note';
+      beforeEach(() => {
+        app.find('FormControl').simulate('change', {
+            target: { value: testNote }
+        });
+      });
+
+      it('should update the text in state', () => {
+          expect(app.state().text).toEqual(testNote);
+      });
+      describe('and submitting the new note', () => {
+          beforeEach(() => {
+              app.find('.btn').at(0).simulate('click');
+          })
+          it('should add the new note to state', () => {
+              expect(app.state().notes[0].text).toEqual(testNote);
+          });
+      });
+      describe('and clicking the clear button', () => {
+          beforeEach(() => {
+              app.find('.btn').at(1).simulate('click');
+          });
+          it('should clear the notes in the state', () => {
+              expect(app.state().notes).toEqual([]);
+          });
+      });
+    });
+      
 })
